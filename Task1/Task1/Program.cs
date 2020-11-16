454b156192b40d39337bf830a14778f98cbd2a77
Александр Зимогоров <zimniimars@gmail.com>
2020-11-16 20:53:18 +0300

    Task_3

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] maxAray = new int[13];
+            Random rnd = new Random();
+            for (int i = 0; i < maxAray.Length; i++)
+            {
+                maxAray[i] = rnd.Next(1, 99);
+            }
+            int maxValue = maxAray[0];
+            for (int i = 1; i < maxAray.Length; i++)
+            {
+                if (maxAray[i] > maxValue)
+                {
+                    maxValue = maxAray[i];
+                }
+            }
+            Console.WriteLine(maxValue);
+            
+        }
+    }
+}
